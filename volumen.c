d93145ff7511df0b9d032ea9400b300cd1e9713e
hahs <153156791+hahs-git@users.noreply.github.com>
2023-12-07 20:15:49 -0700

    Update volume.c

--- volumen.c
+++ volumen.c
@@ -10,51 +10,51 @@ void poli(float *ar);
 int main (void) {
 		char vol;
 		float ar,ext;
-		printf("** Cubo = c / Esfera = o / Prisma = p / Piramide = v **\n");
-		printf("Volumen: ");
+		printf("** Square = s / Circle = c / Prism = p / Pyramid = v **\n");
+		printf("Figure: ");
 		scanf("%c",&vol);
-		if (vol=='c') {
+		if (vol=='s') {
 				float x[1];
-				printf("Lado: ");
+				printf("Side size: ");
 				scanf("%f",&x[0]);
 				x[1]=powf(x[0],3);
-				printf("Volumen: %.2lf m3\n",x[1]);
+				printf("Volume: %.2lf m3\n",x[1]);
 		}
-		else if (vol=='o') {
+		else if (vol=='c') {
 				float y[3];
-				printf("Radio: ");
+				printf("Radius: ");
 				scanf("%f",&y[0]);
 				y[1]=powf(y[0],3);
 				y[2]=(4*pi)*(y[1]);
-				printf("Volumen: %.2lf m3\n",y[2]);
+				printf("Volume: %.2lf m3\n",y[2]);
 		}
 		else if (vol=='p') {
 				int base;
 				float h;
-				printf("Altura: ");
+				printf("Height: ");
 				scanf("%f",&h);
 				printf("Base:\n");
-				printf("Cuadrada = 1 / Triangular = 2 / Circular = 3 / Poligonal = 4\n");
+				printf("Square = 1 / Triangle = 2 / Circle = 3 / Polygon = 4\n");
 				scanf("%i",&base);
 				if (base==1) {
 						cuad(&ar);	
 						ext=ar*h;
-						printf("Volumen: %.2f m3\n",ext);
+						printf("Volume: %.2f m3\n",ext);
 				}
 				else if (base==2) {
 						trian(&ar);
 						ext=ar*h;
-						printf("Volumen: %.2f m3\n",ext);
+						printf("Volume: %.2f m3\n",ext);
 				}
 				else if (base==3) {
 						circ(&ar);
 						ext=ar*h;
-						printf("Volumen: %.2f m3\n",ext);
+						printf("Volume: %.2f m3\n",ext);
 				}
 				else if (base==4) {
 						poli(&ar);
 						ext=ar*h;
-						printf("Volumen: %.2f m3\n",ext);
+						printf("Volume: %.2f m3\n",ext);
 				}
 				else {
 						printf("?!\n");
@@ -64,30 +64,30 @@ int main (void) {
 		else if (vol=='v') {
 				int base;
 				float h;
-				printf("Altura: ");
+				printf("Height: ");
 				scanf("%f",&h);
 				printf("Base:\n");
-				printf("Cuadrada = 1 / Triangular = 2 / Circular = 3 / Poligonal = 4\n");
+				printf("Square = 1 / Triangle = 2 / Circle = 3 / Polygon = 4\n");
 				scanf("%i",&base);
 				if (base==1) {
 						cuad(&ar);	
 						ext=(ar*h)/3;
-						printf("Volumen: %.2f m3\n",ext);
+						printf("Volume: %.2f m3\n",ext);
 				}
 				else if (base==2) {
 						trian(&ar);
 						ext=(ar*h)/3;
-						printf("Volumen: %.2f m3\n",ext);
+						printf("Volume: %.2f m3\n",ext);
 				}
 				else if (base==3) {
 						circ(&ar);
 						ext=(ar*h)/3;
-						printf("Volumen: %.2f m3\n",ext);
+						printf("Volume: %.2f m3\n",ext);
 				}
 				else if (base==4) {
 						poli(&ar);
 						ext=(ar*h)/3;
-						printf("Volumen: %.2f m3\n",ext);
+						printf("Volume: %.2f m3\n",ext);
 				}
 				else {
 						printf("?!\n");
@@ -103,9 +103,9 @@ int main (void) {
 
 void cuad (float *ar){
 		float x[2];
-		printf("Lado x: ");
+		printf("Side x size: ");
 		scanf("%f",&x[0]);
-		printf("Lado y: ");
+		printf("Side y size: ");
 		scanf("%f",&x[1]);
 		*ar = x[0]*x[1];
 }
@@ -114,24 +114,24 @@ void trian (float *ar){
 		float x[2];
 		printf("Base: ");
 		scanf("%f",&x[0]);
-		printf("Altura: ");
+		printf("Height: ");
 		scanf("%f",&x[1]);
 		*ar = (x[0]*x[1])/2;
 }
 void circ (float *ar){
 		float x;
-		printf("Radio: ");
+		printf("Radius: ");
 		scanf("%f",&x);
 		*ar = pi * powf(x,2);
 }
 void poli (float *ar){
 		float x[4];
-		printf("Lados: ");
+		printf("Number of sides: ");
 		scanf("%f",&x[0]);
-		printf("Lado: ");
+		printf("Side size: ");
 		scanf("%f",&x[1]);
 		x[2]= x[0]*x[1];
-		printf("Apotema: ");
+		printf("Apothem: ");
 		scanf("%f",&x[3]);
 		*ar = (x[2]*x[3])/2;
 }
